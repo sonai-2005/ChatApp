e0c8dad4202ccac5fd438cf4d88588a2a242f55b
sonai-2005 <nandisonai60@gmail.com>
2025-08-04 17:07:17 +0530

    fix the accidental error

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async()=>{
     try {
-        const conn = await mongoose.connect("mongodb+srv://nandisonai848:86FhPEnJKtv2bt2U@cluster0.xcr9hpk.mongodb.net/chat_db?retryWrites=true&w=majority&appName=Cluster0");
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`mongo db connected with--: ${conn.connection.host}`);
     } catch (error) {
         console.log("something wrong with the db.js file"+ error);
