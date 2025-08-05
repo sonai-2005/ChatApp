605c088c62e8dc677552a67ccde42ad99dda3abd
sonai-2005 <nandisonai60@gmail.com>
2025-08-05 20:03:30 +0530

    build: updated frontend build output

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -1,10 +1,12 @@
-import {v2 as cloudinary } from "cloudinary"
-import {config} from "dotenv"
+import { v2 as cloudinary } from "cloudinary";
+import { config } from "dotenv";
 
 config();
+
 cloudinary.config({
-    cloud_name:process.env.CLOUDE_NAME,
-    api_key:process.env.CLOUDE_API_KEY,
-    api_secret:process.env.CLOUDE_API_SECRET
-})
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 export default cloudinary;
