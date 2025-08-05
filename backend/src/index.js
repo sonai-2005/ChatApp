9d71bd2da3019b167632f250914f8aa628b5ea05
sonai-2005 <nandisonai60@gmail.com>
2025-08-05 20:11:32 +0530

    debug: list all express endpoints

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,8 +34,12 @@ if(process.env.NODE_ENV==="production"){
         res.sendFile(path.join(__dirname , "../frontend", "dist" , "index.html"))
     })
 }
+console.log("✅ ROUTES LOADED:");
+console.log(listEndpoints(app));
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
   connectDB();
 });
+import listEndpoints from "express-list-endpoints";
+
